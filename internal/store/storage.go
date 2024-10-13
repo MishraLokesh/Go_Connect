648d65ac764b4aee844b2551edbaf5188a323eda
MishraLokesh <4man.mishra@gmail.com>
2024-10-13 13:18:14 +0530

    added query and struct for Users

--- internal/store/storage.go
+++ internal/store/storage.go
@@ -10,14 +10,14 @@ type Storage struct {
 		Create(context.Context, *Post) error
 	}
 	Users interface {
-		Create(context.Context) error
+		Create(context.Context, *User) error
 	}
 }
 
 func NewStorage(db *sql.DB) Storage {
 	return Storage{
-		Posts: &PostsStore{db},
-		Users: &UsersStore{db},
+		Posts: &PostStore{db},
+		Users: &UserStore{db},
 	}
 }
 
