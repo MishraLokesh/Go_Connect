4f7083cf07cd04b54dd92fcb5c39cbb971d622f5
MishraLokesh <4man.mishra@gmail.com>
2024-10-13 14:47:54 +0530

    created storage layer

--- cmd/api/api.go
+++ cmd/api/api.go
@@ -12,11 +12,19 @@ import (
 
 type application struct {
 	config config
-	store store.Storage
+	store  store.Storage
 }
 
 type config struct {
 	addr string
+	db   dbConfig
+}
+
+type dbConfig struct {
+	addr         string
+	maxOpenConns int
+	maxIdleConns int
+	maxIdleTime  string
 }
 
 func (app *application) mount() *chi.Mux {
@@ -31,9 +39,9 @@ func (app *application) mount() *chi.Mux {
 	r.Use(middleware.Recoverer)
 
 	// Set a timeout value on the request context (ctx), that will signal
-  // through ctx.Done() that the request has timed out and further
-  // processing should be stopped.
-  r.Use(middleware.Timeout(60 * time.Second))
+	// through ctx.Done() that the request has timed out and further
+	// processing should be stopped.
+	r.Use(middleware.Timeout(60 * time.Second))
 
 	r.Get("/v1/health", app.healthCheckHandler)
 
