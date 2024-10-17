597a09287c89f15ad50adf78fcf11863aa7fb9ce
MishraLokesh <4man.mishra@gmail.com>
2024-10-17 11:29:12 +0530

    SQL migrations enabled

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -12,10 +12,10 @@ func main() {
 	cfg := &config{
 		addr: env.GetString("ADDR", ":8080"),
 		db: dbConfig{
-			addr: env.GetString("DB_ADDR", "postgres://admin:adminpassword@localhost/socialnetwork?sslmode=disable"),
+			addr:         env.GetString("DB_ADDR", "postgres://admin:adminpassword@localhost/socialnetwork?sslmode=disable"),
 			maxOpenConns: env.GetInt("DB_MAX_OPEN_CONNS", 30),
 			maxIdleConns: env.GetInt("DB_MAX_IDLE_CONNS", 30),
-			maxIdleTime: 			env.GetString("DB_MAX_IDLE_TIME", "15m"),
+			maxIdleTime:  env.GetString("DB_MAX_IDLE_TIME", "15m"),
 		},
 	}
 
@@ -26,7 +26,7 @@ func main() {
 
 	defer db.Close()
 	log.Print("Database connection pool established")
-	
+
 	store := store.NewStorage(db)
 
 	app := &application{
@@ -36,4 +36,3 @@ func main() {
 	mux := app.mount()
 	log.Fatal(app.run(mux))
 }
-
