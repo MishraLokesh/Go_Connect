a00ca899baab070541e4fddd18f88018ff65258c
MishraLokesh <4man.mishra@gmail.com>
2024-10-20 15:08:09 +0530

    Creating and Getting a post from the database updated

--- cmd/api/api.go
+++ cmd/api/api.go
@@ -43,7 +43,16 @@ func (app *application) mount() *chi.Mux {
 	// processing should be stopped.
 	r.Use(middleware.Timeout(60 * time.Second))
 
-	r.Get("/v1/health", app.healthCheckHandler)
+	r.Route("/v1", func(r chi.Router) {
+		r.Get("/health", app.healthCheckHandler)
+
+		r.Route("/posts", func(r chi.Router) {
+			r.Post("/", app.createPostHandler)
+			r.Route("/{postID}", func(r chi.Router) {
+				r.Get("/", app.getPostHandler)
+			})
+		})
+	})
 
 	return r
 }
